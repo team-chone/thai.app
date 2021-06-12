06770e790c9c859a520f44b39795b0b0609d8016
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-06-12 13:56:35 +0900

    長さ途中

--- src/views/new2map.vue
+++ src/views/new2map.vue
@@ -35,9 +35,11 @@
       >
         <p style="color: #000">
           {{ marker.title }}
+          {{ marker.position }}
         </p>
       </GmapInfoWindow>
     </GmapMap>
+    <button v-on:click="nagasa">{{ nagasa }}</button>
     <!-- </v-card>
     </v-col>
   </v-row> -->
@@ -51,6 +53,7 @@ export default {
       maplocation: { lng: 0, lat: 0 },
       zoom: 16,
       i: 0,
+      nagasa: "長さ",
       styleMap: {
         width: "100%",
         height: "400px",
@@ -76,6 +79,7 @@ export default {
           pinicon: {
             url: require("../image/blue-dot.png"),
             scaledSize: { width: 40, height: 40, f: "px", b: "px" },
+            // animation: window.google.maps.Animation.DROP,
           },
         },
         {
@@ -99,6 +103,7 @@ export default {
     this.markers.push({
       title: "現在地",
       position: this.maplocation,
+      // animation: window.google.maps.Animation.DROP,
     })
   },
   methods: {
@@ -117,8 +122,36 @@ export default {
       this.markers.push({
         title: "mark" + this.i,
         position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
+        animation: window.google.maps.Animation.DROP,
       })
       this.i += 1
+    },
+    haversine_distance(mk1, mk2) {
+      const R = 6371.071 // Radius of the Earth in kilometers
+      const rlat1 = mk1.position.lat() * (Math.PI / 180)
+      // Convert degrees to radians
+      const rlat2 = mk2.position.lat() * (Math.PI / 180)
+      // Convert degrees to radians
+      const difflat = rlat2 - rlat1 // Radian difference (latitudes)
+      const difflon =
+        (mk2.position.lng() - mk1.position.lng()) * (Math.PI / 180) // Radian difference (longitudes)
+
+      const d =
+        2 *
+        R *
+        Math.asin(
+          Math.sqrt(
+            Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+              Math.cos(rlat1) *
+                Math.cos(rlat2) *
+                Math.sin(difflon / 2) *
+                Math.sin(difflon / 2)
+          )
+        )
+      return d
+    },
+    nagasa() {
+      this.nagasa = "二つのマーカーを選んで"
     },
   },
 }
