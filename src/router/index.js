cb5679ca63a26a4236eddff7c92de7edcb3601d3
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-06-16 21:03:25 +0900

    ルーター途中

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,38 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
-import Home from "../views/Home.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "mibet",
+    component: () => import("../views/mibet.vue"),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/makeacount",
+    name: "Makeacount",
+    component: () => import("../views/Makeacount.vue"),
   },
   {
-    path: "/map",
-    name: "Map",
-    component: () => import("../views/new2map.vue"),
+    path: "/googleacount",
+    name: "googleacount",
+    component: () => import("../views/googleacount.vue"),
+  },
+  {
+    path: "/otheracount",
+    name: "otheracount",
+    component: () => import("../views/otheracount.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/maphome",
+    name: "maphome",
+    component: () => import("../views/maphome.vue"),
   },
 ]
 
