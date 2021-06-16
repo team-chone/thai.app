91cc36592ab27bbf94526244096f1360a0cf8b7e
retzky19 <retzky39@gmail.com>
2021-06-16 23:26:29 +0900

    ログイン機能の実験

--- src/views/mibet.vue
+++ src/views/mibet.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <h1>ホーム画面</h1>
-    <router-link to="/makeacount">アカウント作成</router-link> |
+    <button>アカウント作成</button>
     <router-link to="/login">ログイン</router-link>
+    <button @click="check">確認</button>
   </div>
 </template>
+<script>
+// import firebase from "firebase"
+
+// export default {
+// methods: {
+//   logIn() {
+//     const provider = new firebase.auth.GoogleAuthProvider()
+//     firebase.auth().signInWithPopup(provider).then(
+//       firebase.firestore().collection("users").doc(this.$auth.uid).set({
+//         nicknamE: this.nickname
+//         agE: this.age
+//         gendeR: this.gender
+//       })
+//     )
+//   },
+// },
+// }
+export default {
+  methods: {
+    check() {
+      if (this.$auth.currentUser) {
+        console.log("ログインしてるよ")
+      } else {
+        console.log("ログインしてないよ")
+      }
+    },
+  },
+}
+</script>
