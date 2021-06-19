bb3f7668c051dfc3ad94ad7a92ed9f7cc9e0e812
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-06-19 19:30:53 +0900

    位置情報渡す

--- src/views/user/wolet.vue
+++ src/views/user/wolet.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h1>ウォレット</h1>
+    <div>あなたの今のポイントは{{ point }}</div>
     <router-link to="/maphome">戻る</router-link>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      point: "0",
+    }
+  },
+}
+</script>
