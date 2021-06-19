c728231920095bddc46a277cab9346ca23b1ab54
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-06-19 14:10:36 +0900

    company map

--- src/views/commaphome.vue
+++ src/views/commaphome.vue
@@ -3,5 +3,205 @@
     <h1>会社mapホーム画面</h1>
     <router-link to="/combuildpin">ピンを立てる</router-link> |
     <router-link to="/compinview">ピンを見る</router-link>
+
+    <div id="app">
+      <!--ハンバーガーメニューのボタン-->
+      <div class="hamburger_btn" v-on:click="ActiveBtn = !ActiveBtn">
+        <span
+          class="line line_01"
+          v-bind:class="{ btn_line01: ActiveBtn }"
+        ></span>
+        <span
+          class="line line_02"
+          v-bind:class="{ btn_line02: ActiveBtn }"
+        ></span>
+        <span
+          class="line line_03"
+          v-bind:class="{ btn_line03: ActiveBtn }"
+        ></span>
+      </div>
+      <!--サイドバー-->
+      <transition name="menu">
+        <div class="menu" v-show="ActiveBtn">
+          <ul>
+            <li><router-link to="/acount">アカウント</router-link></li>
+            <li><a href="#">(受信トレイ)</a></li>
+          </ul>
+        </div>
+      </transition>
+    </div>
+    <GmapMap
+      ref="gmp"
+      map-type-id="roadmap"
+      :center="maplocation"
+      :zoom="zoom"
+      :style="styleMap"
+      :options="mapOptions"
+      @click="mark($event)"
+    >
+      <GmapMarker
+        v-for="(m, index) in markers"
+        :key="index"
+        :title="m.title"
+        :position="m.position"
+        :clickable="true"
+        :draggable="false"
+        :icon="m.pinicon"
+        :range="m.range"
+        @click="onClickMarker(index, m)"
+      />
+      <GmapInfoWindow
+        :options="infoOptions"
+        :position="infoWindowPos"
+        :opened="infoWinOpen"
+        @closeclick="infoWinOpen = false"
+      >
+        <p style="color: #000">
+          {{ marker.title }}
+          {{ marker.position }}
+        </p>
+      </GmapInfoWindow>
+    </GmapMap>
   </div>
 </template>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script>
+export default {
+  data() {
+    return {
+      ActiveBtn: false,
+      styleMap: {
+        width: "100%",
+        height: "400px",
+      },
+      mapOptions: {
+        streetViewControl: false,
+        styles: [],
+      },
+      infoOptions: {
+        minWidth: 200,
+        pixelOffset: {
+          width: 0,
+          height: -35,
+        },
+      },
+    }
+  },
+  async mounted() {
+    const currentPosTmp = await this.getCurrentPosition()
+    const currentPos = {
+      lat: currentPosTmp.coords.latitude,
+      lng: currentPosTmp.coords.longitude,
+    }
+    this.maplocation = currentPos
+    this.markers.push({
+      title: "mark0(現在地)",
+      position: this.maplocation,
+    })
+    this.i += 1
+  },
+  methods: {
+    mark(event) {
+      this.markers.push({
+        title: "mark" + this.i,
+        position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
+        range: 150,
+        pinicon: {
+          url: require("../image/green-dot.png"),
+          scaledSize: { width: 40, height: 40, f: "px", b: "px" },
+        },
+      })
+      this.i += 1
+    },
+  },
+}
+</script>
+
+<style>
+/*ボタン*/
+.hamburger_btn {
+  position: fixed; /*常に最上部に表示したいので固定*/
+  top: 0;
+  right: 0;
+  width: 70px;
+  height: 72px;
+  cursor: pointer;
+  z-index: 50;
+}
+
+.hamburger_btn .line {
+  position: absolute;
+  top: 0;
+  left: 20px;
+  width: 32px;
+  height: 2px;
+  background: #333333;
+  text-align: center;
+}
+
+.hamburger_btn .line_01 {
+  top: 16px;
+  transition: 0.4s ease;
+}
+.hamburger_btn .line_02 {
+  top: 26px;
+  transition: 0.4s ease;
+}
+.hamburger_btn .line_03 {
+  top: 36px;
+  transition: 0.4s ease;
+}
+
+.btn_line01 {
+  transform: translateY(10px) rotate(-45deg);
+  transition: 0.4s ease;
+}
+.btn_line02 {
+  transition: 0.4s ease;
+  opacity: 0;
+}
+.btn_line03 {
+  transform: translateY(-10px) rotate(45deg);
+  transition: 0.4s ease;
+}
+
+/*サイドバー*/
+.menu-enter-active,
+.menu-leave-active {
+  transition: opacity 0.4s;
+}
+.menu-enter,
+.menu-leave-to {
+  opacity: 0;
+}
+.menu-leave,
+.menu-enter-to {
+  opacity: 1;
+}
+
+.menu li {
+  list-style: none;
+  line-height: 1;
+  padding: 1rem;
+}
+.menu {
+  background-color: rgba(197, 197, 197, 0.671);
+  z-index: 30;
+  padding: 2rem 1rem;
+  position: fixed;
+  width: 20rem;
+  height: 80rem;
+  top: 0;
+  right: 0;
+}
+.menu a {
+  color: rgb(66, 66, 66);
+  text-decoration: none;
+  font-size: 1.2rem;
+  padding: 0 2rem;
+}
+.menu ul {
+  margin: 1rem;
+  padding: 0;
+}
+</style>
