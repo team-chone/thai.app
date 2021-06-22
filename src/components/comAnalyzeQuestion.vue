2f31cc56b192672b92fb05bf92824a2f8db7c303
sotaurabe <tacchoso@icloud.com>
2021-06-22 17:24:06 +0900

    アンケートの新規作成、編集、分析をコンポーネントで切り出した

---
+++ src/components/comAnalyzeQuestion.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>アンケート結果</h2>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ src/components/comEditQuestion.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>アンケートの編集</h2>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase"
+export default {
+  data() {
+    return {
+      pin_id: String(this.$route.params.pin_id),
+      pin_name: this.$route.params.pin_name,
+      //   questionnaire: {
+      //     questionnaire_title: "",
+      //     questionnaire_limit: "",
+      //     questions: [
+      //       {
+      //         question_title: "",
+      //         question_type1: "",
+      //         question_type2: "",
+      //         selects: [],
+      //       },
+      //     ],
+      //   },
+      pin_data: {},
+      question_select: "",
+    }
+  },
+
+  created() {
+    firebase
+      .firestore()
+      .collection("pins")
+      .doc(this.pin_id)
+      .get()
+      .then((doc) => {
+        this.pin_data.push({
+          id: doc.id,
+          ...doc.data(),
+        })
+        console.log(this.pin_data)
+      })
+  },
+}
+</script>
