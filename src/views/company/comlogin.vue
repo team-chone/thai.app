7294461b7b0b6664b49c7328a93a05b0224af0ab
retzky19 <retzky39@gmail.com>
2021-06-24 19:21:49 +0900

    企業ログイン完成

---
+++ src/views/company/comlogin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="input">
+    <div>
+      メールアドレス：
+      <input type="text" v-model="comMail" />
+    </div>
+    <div>
+      パスワード：
+      <input type="text" v-model="comPassword" />
+    </div>
+    <br />
+    <button @click="signIn">ログイン</button>
+    {{ message }}
+  </div>
+</template>
+<script>
+import firebase from "firebase"
+
+export default {
+  data() {
+    return {
+      comMail: "",
+      comPassword: "",
+      message: "",
+    }
+  },
+  methods: {
+    signIn() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.comMail, this.comPassword)
+        .then(() => {
+          this.$router.push("/commaphome")
+        })
+        .catch(() => {
+          this.message = "パスワードまたはメールアドレスが間違っています"
+        })
+    },
+  },
+}
+</script>
